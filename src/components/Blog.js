28942678e10d6214a5333521e8da81d67124992a
thayawee <thayaweepinta@gmail.com>
2023-06-07 00:58:18 +0700

    h-8

---
+++ src/components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Classes from './Blog.module.css'
+const Blog = () => {
+  return (
+    <div className={Classes.card}>
+    <img src="copy-writing.png" alt="logo-blog"/>
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Classes from './Navbar.module.css'
+const Navbar = () => {
+  return (
+    <div className={Classes.card}>
+        <p>CLEVERSE</p>
+        <a href="https://www.google.co.th/">About Us</a>
+        <a href="https://www.google.co.th/">Blog</a>
+        <a href="https://www.google.co.th/">Career</a>
+        <a href="https://www.google.co.th/">Contact Us</a>
+    </div>
+  )
+}
+
+export default Navbar
